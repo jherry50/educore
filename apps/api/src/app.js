94881f25192b149b01f1021d2c1bfa9e5155d1e9
Bolarinwa Ilesanmi <bhola050@gmail.com>
2026-08-03 10:43:17 +0100

    chore: initialize EduCore SMS repository

---
+++ apps/api/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+
+res.json({
+message:"EduCore SMS API Running"
+});
+
+});
+
+
+export default app;
---
+++ docs/architecture.md
@@ -0,0 +1,17 @@
+# EduCore SMS Architecture
+
+EduCore SMS is a modern School Management System.
+
+## Stack
+
+Frontend:
+React + Vite
+
+Backend:
+Node.js + Express
+
+Database:
+MongoDB
+
+Architecture:
+Feature-based Clean Architecture
