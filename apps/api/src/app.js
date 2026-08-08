058936f1f54a0c29277b78bee277513e5a7096a4
Bolarinwa Ilesanmi <bhola050@gmail.com>
2026-08-08 20:07:22 +0100

    Feat: Update folder structure for backend

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -2,27 +2,31 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import compression from "compression";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-
-app.use(cors());
-
 app.use(helmet());
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(compression());
 app.use(morgan("dev"));
-
 app.use(express.json());
+app.use(cookieParser());
 
-
-app.get("/",(req,res)=>{
-
-res.json({
-message:"EduCore SMS API Running"
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "EduCore API is running",
+    timestamp: new Date().toISOString(),
+  });
 });
-
-});
-
 
 export default app;
--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,13 +1,13 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDatabase } from "./config/database.js";
 
+dotenv.config();
+
+await connectDatabase(process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT,()=>{
-
-console.log(
-`EduCore API running on port ${PORT}`
-);
-
+app.listen(PORT, () => {
+  console.log(`🚀 EduCore API running on http://localhost:${PORT}`);
 });
